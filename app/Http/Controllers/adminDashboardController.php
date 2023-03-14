d60345b5c5afdb74046a7b44b0c7c59c79e05bde
antol <aloysiusluilui@gmail.com>
2023-03-14 15:02:56 +0700

    count di dashboard admin

--- app/Http/Controllers/adminDashboardController.php
+++ app/Http/Controllers/adminDashboardController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminDashboardController extends Controller
 {
     public function index()
     {
-        return view('adminDashboard');
+        $countPost = Post::count();
+        $countUser = User::count();
+        return view('adminDashboard', compact('countPost', 'countUser'));
     }
 }
